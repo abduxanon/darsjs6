0886688e9fd9a5734ccfe113740516402fcd1220
abduxanon <abduxanonaxmadaliyev@gmail.com>
2024-04-07 08:33:20 +0500

    dars

---
+++ app.js
@@ -0,0 +1,53 @@
+// let btn = document.querySelector("#btn");
+// let di = document.querySelector("#di");
+// let bg1, bg2, bg3;
+// btn.addEventListener("click", function () {
+//   bg1 = Math.floor(Math.random() * 255);
+//   bg2 = Math.floor(Math.random() * 255);
+//   bg3 = Math.floor(Math.random() * 255);
+
+//   di.style.backgroundColor = `rgb(${bg1}, ${bg2}, ${bg3})`;
+// });
+
+// function randomColor() {
+//   let randomcolor1 = Math.floor(Math.random() * 255);
+//   let randomcolor2 = Math.floor(Math.random() * 255);
+//   let randomcolor3 = Math.floor(Math.random() * 255);
+//   return `rgb(${randomcolor1},${randomcolor2},${randomcolor3})`;
+// }
+// section.style.backgroundColor = `linear-gradient(180deg,${randomcolor1}, ${randomcolor2}, ${randomcolor3})`;;
+// body.style.backgroundImage = `linear-gradient(180deg,${randomcolor1}, ${randomcolor2}, ${randomcolor3})`;
+
+//  let minut = 0;
+//  let secuds = 0;
+//  let soat = 0;
+
+//  let inreval = setInterval(()=>{
+//     secuds += 1;
+
+//     title.textContent = `${minut < 10 ? `0${minut}`: minut}:${secuds < 10 ? `0${secuds}`: secuds}`;
+
+// });
+// let button1 = document.querySelector("#btn");
+// let button2 = document.querySelector("#btn2");
+// let title = document.querySelector("#title");
+
+// function btnClick() {
+//   // let h2 = button1.nextElementSibling.nextElementSibling;
+//   if (+title.textContent == 20) {
+//     alert("tugadi");
+//   } else {
+//     title.textContent++;
+//   }
+// }
+// button1.setAttribute("onclick", "btnClick()");
+
+// function btnClick2() {
+//   // let h2 = button2.nextElementSibling;
+//   if (+title.textContent == 1) {
+//     alert("tugadi");
+//   } else {
+//     title.textContent--;
+//   }
+// }
+// button2.setAttribute("onclick", "btnClick2()");
